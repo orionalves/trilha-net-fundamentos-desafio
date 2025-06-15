b7d65beb49749bd6853f4d1b329ed73b323d647c
Orion Alves <oriondecarvalho@gmail.com>
2025-06-15 19:58:19 -0300

    TODO: Realizar um laço de repetição, exibindo os veículos estacionados.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -54,8 +54,10 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
